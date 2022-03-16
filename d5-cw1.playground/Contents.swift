58f322a175f77f1d1519b636cd3a28b7b06fc9c8
fatamah <fatiabbasali2018@gmail.com>
2022-03-16 18:48:54 +0300

    zahra34

--- d5-cw1.playground/Contents.swift
+++ d5-cw1.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let family = [ "fatemeh" , "hawraa", "ali","jaber","mahdi","sadeq","zahra"]
+print(family.count)
+
+var evenNumbers = [2,4,6,8,10,12]
+
+evenNumbers.remove(at: 2)
+print(evenNumbers)
+evenNumbers += [16,18,20,22]
+ print(evenNumbers)
+
+var money = [1.50,2.250,6.750]
+money.randomElement()
+money.removeAll()
+
+var numbers = [1,2,3,4,5]
+for i in numbers{
+print(i)
+}
